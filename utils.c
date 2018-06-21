ec6c0a03046cc2a295c86d36d4c274a1dca2babc
Santtiago98 <39803285+Santtiago98@users.noreply.github.com>
2018-06-20 23:14:28 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 #include "utils.h"
+#include "mp3.h"
 
 void swap (void *a, void *b)
 {
     void * aux;
-    
-    if(*a==*b)
-        break;
+///No se puede  poner el if ese que habia puesto antes pq son punteros void
     aux = a;
     a = b;
     b = aux;
+}
+
+int ADT_Track_compare_by_artist(const void *e1, const void *e2)
+{
+    ADT_Track_t *p1=(ADT_Track_t *)e1;
+    ADT_Track_t *p2=(ADT_Track_t *)e2;
+
+    return strcmp(p1->artist,p2->artist);
+}
+
+int ADT_Track_compare_by_genre(const void *e1, const void *e2)
+{
+    ADT_Track_t *p1=(ADT_Track_t *)e1;
+    ADT_Track_t *p2=(ADT_Track_t *)e2;
+
+    return strcmp(p1->genre,p2->genre);
+}
+
+int ADT_Track_compare_by_name(const void *e1, const void *e2)
+{
+    ADT_Track_t *p1=(ADT_Track_t *)e1;
+    ADT_Track_t *p2=(ADT_Track_t *)e2;
+
+    return strcmp(p1->tag,p2->tag);
 }
