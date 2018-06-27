df073c4b6ef0f2c44095a8e45fc1affde269752d
Santtiago98 <39803285+Santtiago98@users.noreply.github.com>
2018-06-27 01:05:19 -0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,19 +1,27 @@
-/*errors.c*/
+#include <stdio.h>
+
 #include "errors.h"
 
 /*diccioniario de errores*/
 char * errors[MAX_ERRORS]=
-        {
-                "OK",
-                "Puntero nulo",
-                "Argumentos inválidos",
-                "Sin memoria",
-                "No se pudo abrir el archivo",
-                "No se pudo escribir el archivo",
-                "El archivo está corrupto",
-                "No se pudo renombrar el archivo",
-                "No se pudo borrar el archivo",
-                "Arreglo de cadenas inválido",
-                "El registro no existe",
-                "El registro está duplicado"
-        };
+{
+        "OK",
+        "Puntero nulo",
+        "Argumentos inválidos",
+        "Sin memoria",
+        "No se pudo abrir el archivo",
+        "No se pudo escribir el archivo",
+        "El archivo está corrupto",
+        "No se pudo renombrar el archivo",
+        "No se pudo borrar el archivo",
+        "Arreglo de cadenas inválido",
+        "El registro no existe",
+        "El registro está duplicado"
+};
+
+status_t print_errors(status_t st)
+{
+        fprintf(stderr,"%s",errors[st]);
+
+        return st;
+}
