639ebd0666290ea8a1022b99419aef9fff336d66
Santtiago98 <39803285+Santtiago98@users.noreply.github.com>
2018-06-27 01:10:10 -0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -2,10 +2,13 @@
 #define VECTOR__H
 
 #include <stdio.h>
+
+#include "main.h"
 #include "errors.h"
-#include "mp3.h"
 #include "types.h"
+#include "mp3.h"
 #include "utils.h"
+
 
 #define INIT_CHOP 15
 #define CHOP_SIZE 7
@@ -18,6 +21,7 @@ typedef struct
     destructor_t destructor;
     comparator_t comparator;
     printer_t printer;
+
 }ADT_Vector_t;
 
 status_t ADT_Vector_new (ADT_Vector_t **);
@@ -26,8 +30,7 @@ status_t ADT_Vector_set_destructor (ADT_Vector_t *, destructor_t);
 status_t ADT_Vector_set_comparator (ADT_Vector_t *, comparator_t);
 status_t ADT_Vector_append (ADT_Vector_t **, void *);
 status_t ADT_Vector_sort (ADT_Vector_t *);
-status_t ADT_Vector_print_to_xml(FILE *, ADT_Vector_t*);
-status_t ADT_Vector_print_to_csv(FILE*, ADT_Vector_t*);
-
+status_t ADT_Vector_print_to_file(FILE *, ADT_Vector_t*);
+status_t ADT_Vector_set_printer(ADT_Vector_t *p, printer_t pf);
 
 #endif
