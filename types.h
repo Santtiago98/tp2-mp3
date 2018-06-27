12a39f2e931fc6298b014ffe9d5976ed9f628cc6
Santtiago98 <39803285+Santtiago98@users.noreply.github.com>
2018-06-27 01:08:15 -0300

    Update types.h

--- types.h
+++ types.h
@@ -1,6 +1,14 @@
 #ifndef TYPES__H
 #define TYPES__h
 
+#include <stdio.h>
+
+#include "main.h"
+#include "errors.h"
+#include "vector.h"
+#include "mp3.h"
+#include "utils.h"
+
 #define MAX_FORMATS 3
 #define MAX_SORTING_CRITERION 3
 #define MAX_OUTPUT_FORMAT 2
@@ -14,11 +22,27 @@ char * formats[MAX_FORMATS]=
 
 char * sort_criterion[MAX_SORTING_CRITERION] =
         {
-                "name",
+                "title",
                 "artist",
                 "genre"
         };
 
-typedef status_t (*destructor_t) (void **);
-typedef int (*comparator_t) (const void *, const void *);
-typedef status_t (*printer_t) (void *);
+int (*comparators[MAX_SORTING_CRITERION])(const void *,const void *)=
+        {
+                compare_by_title,
+                compare_by_artist,
+                compare_by_genre
+        };
+
+status_t (* export_as[MAX_FORMATS]) (FILE * fo, void * track)=
+        {
+                ADT_Track_print_to_csv,
+                ADT_Track_print_to_xml,
+                ADT_Track_print_to_html
+        };
+
+typedef status_t (* destructor_t) (void **);
+typedef int (* comparator_t) (const void *, const void *);
+typedef status_t (* printer_t) (FILE * fo, void * p);
+
+#endif
